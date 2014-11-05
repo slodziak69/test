bd3c7bc19c4efe58a3a65bc325262f357da8fd36
Mateusz Stelmaszuk <emstel@MacBook-Pro-Mateusz.local>
2014-11-05 19:03:26 +0100

    testing geolocation

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    @IBOutlet var mapKit: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        // 1
+        let location = CLLocationCoordinate2D(
+            latitude: 53.1316956,
+            longitude: 23.1571699
+        )
+        // 2
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapKit.setRegion(region, animated: true)
+        
+        //3
+        let annotation = MKPointAnnotation()
+        annotation.setCoordinate(location)
+        annotation.title = "Bialystok"
+        annotation.subtitle = "Ratuszowa"
+        mapKit.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
